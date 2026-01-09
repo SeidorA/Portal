7377acb86052817f5adae05de12a3b871f37e4cb
JuanToSeidor <jdtorres@seidoranalytics.com>
2026-01-09 16:59:51 -0300

    changes un base URL

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -58,7 +58,7 @@ const config: Config = {
   // Set the production url of your site here
   url: 'https://products.seidoranalytics.com/',
   // Set the /<baseUrl>/ pathname under which your site is served
-  baseUrl: '/',
+  baseUrl: '',
 
   // Deployment settings: GitHub Pages specific fields removed becausenp
   // this project no longer uses GitHub Pages.
